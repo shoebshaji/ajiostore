597062299911df165e916e073f06b96259721fd3
Mohammed Shoeb Shaji <88362670+shoebshaji@users.noreply.github.com>
2022-04-06 17:27:46 +0530

    8 - backend auth(users) - controllers - middlewares - routes

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -3,6 +3,7 @@
 const notFound = (req, res, next) => {
    const error = new Error(`Not Found - ${req.originalUrl}`)
    res.status(404)
+   next(error)
 }
 
 const errorHandle = (err, req, res, next) => {
--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,24 @@
 import express from 'express'
 import dotenv from 'dotenv'
-// import products from './data/products.js'
-import productRoutes from './routes/productRoutes.js'
+import colors from 'colors'
 import { notFound, errorHandle } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
-connectDB()
+import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
 
+connectDB()
+
+
 const app = express()
+app.use(express.json()) //accept json
 
 app.get('/', (req, res) => {
   res.send('API is running....')
 })
 
 app.use('/api/products', productRoutes)
-app.use('/api/products/:id', productRoutes)
 app.use(notFound)
 app.use(errorHandle)
 
@@ -25,5 +27,5 @@ const PORT = process.env.PORT || 5000
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
 )
