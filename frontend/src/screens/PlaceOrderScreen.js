ff64eb796cd147f826b20934dabad1b841b70a1d
Mohammed Shoeb Shaji <88362670+shoebshaji@users.noreply.github.com>
2022-04-13 17:50:30 +0530

    12.1 ordercontroller-routes-store

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { Button, Row, Col, ListGroup, Image, Card } from 'react-bootstrap'
+import {useDispatch, useSelector} from 'react-redux'
+import Message from '../components/Message'
+import CheckoutSteps from '../components/CheckoutSteps'
+
+import { createOrder } from '../actions/orderActions'
+import { ORDER_CREATE_RESET } from '../constants/orderConstants'
+import { ORDER_DETAILS_RESET } from '../constants/orderConstants'
+
 
 function PlaceOrderScreen() {
+  const dispatch = useDispatch()
+
+  const cart = useSelector((state) => state.cart)
   return (
-    <div></div>
+    <>
+
+
+    </>
   )
 }
 
