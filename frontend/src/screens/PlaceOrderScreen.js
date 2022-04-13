3fe9ba6bf32bba8fdb0af8273b4e56ae15231cda
Mohammed Shoeb Shaji <88362670+shoebshaji@users.noreply.github.com>
2022-04-13 16:17:31 +0530

    12 - orderConstants-actions-reducers

---
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PlaceOrderScreen() {
+  return (
+    <div></div>
+  )
+}
+
+export default PlaceOrderScreen
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,6 +4,7 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
 import { userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer } from './reducers/userReducers'
+import { orderCreateReducer } from './reducers/orderCreateReducer'
 
 
 // initialisation of store 
@@ -14,7 +15,9 @@ const reducer = combineReducers({
     userLogin: userLoginReducer,
     userRegister: userRegisterReducer,
     userDetails: userDetailsReducer,
-    userUpdateProfile: userUpdateProfileReducer
+    userUpdateProfile: userUpdateProfileReducer,
+    orderCreate: orderCreateReducer,
+    
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
