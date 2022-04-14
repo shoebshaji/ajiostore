3f0f2044b4e541b93232ebdc8b0ef9e27e3fdf1d
Mohammed Shoeb Shaji <88362670+shoebshaji@users.noreply.github.com>
2022-04-14 17:20:29 +0530

    13 - PlaceOrderScreen-PaymentScreen order:actions,reducers-controller

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -1,5 +1,6 @@
-import {ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_CREATE_FAIL} from '../constants/orderConstants'
+import {ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_CREATE_FAIL, ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS, ORDER_DETAILS_FAIL} from '../constants/orderConstants'
 import axios from 'axios'
+import { logout } from '../actions/userActions'
 
 
 export const createOrder = (order) => async (dispatch, getState) => {
@@ -16,7 +17,7 @@ export const createOrder = (order) => async (dispatch, getState) => {
 
         }
 
-        const { data } =await axios.post(`/api/orders`, order, config)
+        const { data } = await axios.post(`/api/orders`, order, config)
 
         dispatch({ type: ORDER_CREATE_SUCCESS, payload: data })
 
@@ -26,6 +27,37 @@ export const createOrder = (order) => async (dispatch, getState) => {
            ? error.response.data.message
            :error.message
            })
+        
+    }
+}
+
+export const getOrderDetails = (id) => async (dispatch, getState) => {
+    try {
+        dispatch({type: ORDER_DETAILS_REQUEST})
+        const { userLogin: { userInfo }} = getState()
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+
+        }
+
+        const { data } = await axios.post(`/api/orders/${id}`, config)
+
+        dispatch({ type: ORDER_DETAILS_SUCCESS, payload: data})
+
+
+    } catch (error) {
+        const message = error.response && error.response.data.message
+                        ? error.response.data.message
+                        : error.message
+        
+        if(message === 'Not authorized, Token Failed') {
+            dispatch(logout())
+        }
+        dispatch({ type:ORDER_DETAILS_FAIL, payload: message })
         
     }
 }
--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -1,4 +1,4 @@
-import { ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_CREATE_FAIL, ORDER_CREATE_RESET } from '../constants/orderConstants'
+import { ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_CREATE_FAIL, ORDER_CREATE_RESET, ORDER_DETAILS_SUCCESS, ORDER_DETAILS_REQUEST, ORDER_DETAILS_FAIL, ORDER_DETAILS_RESET } from '../constants/orderConstants'
 
 
 export const orderCreateReducer = ( state={}, action ) => {
@@ -11,6 +11,21 @@ export const orderCreateReducer = ( state={}, action ) => {
             return { loading: false, error: action.payload}
         case ORDER_CREATE_RESET:
             return {}
+        default:
+            return state
+    }
+}
+
+export const orderDetailsReducer = ( state={ orderItems: [], shippingAddress: {} } , action ) => {
+    switch(action.type) {
+        case ORDER_DETAILS_REQUEST:
+            return { ...state, loading: true }
+        case ORDER_DETAILS_SUCCESS: 
+            return { loading: false, order: action.payload }
+        case ORDER_DETAILS_FAIL:
+            return { loading: false, error: action.payload}
+         case ORDER_DETAILS_RESET:
+                return {}
         default:
             return state
     }
